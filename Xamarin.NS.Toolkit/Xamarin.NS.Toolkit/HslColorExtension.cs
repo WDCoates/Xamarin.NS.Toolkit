91c4fb91a9ed59aadeee4909e26a592f623c9d3e
DCoates <DCoates@Rubiconsoftware.com>
2019-11-18 08:48:01 +0000

    Adding HslColorExtension Class and extra method for PureBlack

---
+++ Xamarin.NS.Toolkit/Xamarin.NS.Toolkit/HslColorExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.NS.Toolkit
+{
+    public class HslColorExtension : IMarkupExtension
+    {
+        public double H { get; set;}
+        public double S { get; set; }
+        public double L { get; set; }
+        public double A { get; set; }
+
+        public HslColorExtension()
+        {
+            A = 1;
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Color.FromHsla(H, S, L, A);
+        }
+
+        public object PureBlack()
+        {
+            return Color.FromHsla(0, 0, 0);
+        }
+    }
+}
