4a1a8b5138ce676d74302b9b095d1cd0ea1cfbec
DCoates <DCoates@Rubiconsoftware.com>
2019-12-06 09:01:54 +0000

    Read Only BindableProperty

---
+++ Xamarin.NS.Toolkit/Xamarin.NS.Toolkit/CountedLabel.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin.NS.Toolkit
+{
+    public class CountedLabel : Label
+    {
+        static readonly BindablePropertyKey WordCountKey =
+            BindableProperty.CreateReadOnly("WordCount", typeof(int),
+                                            typeof(CountedLabel), 0);
+
+        public static readonly BindableProperty WordCountProperty = WordCountKey.BindableProperty;
+
+        public CountedLabel()
+        {
+
+            //Set the WordCount property when the Text property changes.
+            PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName != "Text") return;
+                WordCount = string.IsNullOrEmpty(Text) ? 0 : Text.Split(' ', '-', '\u2014').Length;
+            };
+
+        }
+
+        public int WordCount
+        {
+            private set => SetValue(WordCountKey, value);
+            get => (int)GetValue(WordCountProperty);
+        }
+    }
+}
