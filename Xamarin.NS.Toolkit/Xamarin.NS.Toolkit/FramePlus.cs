58420d050896a3fc773cb5503717340c92a766c4
DCoates <DCoates@Rubiconsoftware.com>
2019-12-11 09:34:26 +0000

    Extending the Frame  class to have a label as it's content.

---
+++ Xamarin.NS.Toolkit/Xamarin.NS.Toolkit/FramePlus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Xamarin.NS.Toolkit
+{
+    public class FramePlus : Frame
+    {
+        public FramePlus(string text)
+        {
+            Label label = new Label {Text = $"text"};
+
+            this.Content = label;
+        }
+    }
+}
